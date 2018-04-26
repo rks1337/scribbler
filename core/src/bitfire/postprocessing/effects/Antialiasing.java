2d95145f81b25347a05c599b40a381f3337f159d
rks1337 <tudby001@mymail.unisa.edu.au>
2018-04-26 18:15:02 +0930

    k

--- core/src/bitfire/postprocessing/effects/Antialiasing.java
+++
@@ -1,9 +0,0 @@
-
-package bitfire.postprocessing.effects;
-
-import bitfire.postprocessing.PostProcessorEffect;
-
-public abstract class Antialiasing extends PostProcessorEffect {
-
-	public abstract void setViewportSize (int width, int height);
-}
